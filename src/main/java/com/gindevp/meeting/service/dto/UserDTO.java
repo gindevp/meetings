91b1a353485662d7d63c734142644a9eccb0b20c
gindevp <nguyenhuuquyet@07092001@gmail.com>
2026-02-26 19:43:19 +0700

    update

--- src/main/java/com/gindevp/meeting/service/dto/UserDTO.java
+++ src/main/java/com/gindevp/meeting/service/dto/UserDTO.java
@@ -15,14 +15,22 @@ public class UserDTO implements Serializable {
 
     private String login;
 
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+
     public UserDTO() {
         // Empty constructor needed for Jackson.
     }
 
     public UserDTO(User user) {
         this.id = user.getId();
-        // Customize it here if you need, or not, firstName/lastName/etc
         this.login = user.getLogin();
+        this.firstName = user.getFirstName();
+        this.lastName = user.getLastName();
+        this.email = user.getEmail();
     }
 
     public Long getId() {
@@ -39,6 +47,30 @@ public class UserDTO implements Serializable {
 
     public void setLogin(String login) {
         this.login = login;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     @Override
