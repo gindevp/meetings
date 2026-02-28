3a53c0976a45abc532c80f20c8e90a0a9fcc523d
gindevp <nguyenhuuquyet@07092001@gmail.com>
2026-02-28 21:15:24 +0700

    update

--- src/main/java/com/gindevp/meeting/service/UserService.java
+++ src/main/java/com/gindevp/meeting/service/UserService.java
@@ -109,7 +109,7 @@ public class UserService {
             newUser.setEmail(userDTO.getEmail().toLowerCase());
         }
         newUser.setImageUrl(userDTO.getImageUrl());
-        newUser.setLangKey(userDTO.getLangKey());
+        newUser.setLangKey(userDTO.getLangKey() != null ? userDTO.getLangKey() : Constants.DEFAULT_LANGUAGE);
         // new user is not active
         newUser.setActivated(false);
         // new user gets registration key
